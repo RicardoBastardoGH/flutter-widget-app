96f368e37a99fc16d1de094c272bb3d4177d9271
Ricardo Bastardo <rbrodriguez.siaca@gmail.com>
2025-04-11 16:44:24 -0400

    Screens creadas y barril actualizado

---
+++ lib/presentation/screen/snackbar/snackbar_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ScanckbarScreen extends StatelessWidget {
+
+  static const String name = 'snackbar_screen';
+  
+  const ScanckbarScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
